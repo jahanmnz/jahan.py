586c5797ae6bec48051039267f2f2631662311a7
github_username <you@example.com>
2023-07-21 14:56:18 +0500

    code of Assignment Operators

---
+++ Assignment Operators.py
@@ -0,0 +1,39 @@
+x = 3
+
+print (x)
+
+
+x = 24
+x += 39
+
+print(x)
+
+
+x = 55
+x -= 66
+
+print (x)
+
+
+x = 77
+x *= 33
+
+print (x)
+
+
+x = 20
+x /= 4
+
+print(x)
+
+
+x = 56
+x %= 7
+
+print (x)
+
+
+x= 77
+x //= 5
+
+print(x)
