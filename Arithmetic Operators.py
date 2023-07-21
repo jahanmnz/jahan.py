4a1dfcad084a84f9e215d72ac4e82d1d0e8b94f7
github_username <you@example.com>
2023-07-21 14:47:05 +0500

    code of Arithmetic Operators

---
+++ Arithmetic Operators.py
@@ -0,0 +1,40 @@
+x =39 
+y = 49
+
+print(x // y)
+
+
+x = 32
+y = 35 
+
+print(x ** y)
+
+
+x =34 
+y = 43
+
+print(x % y)
+
+
+x = 44
+y = 55
+
+print(x / y )
+
+
+x= 22
+y =24
+
+print(x*y)
+
+
+x = 5
+y = 3
+
+print(x - y)
+
+
+x = 20 
+y = 15
+
+print(x + y)
