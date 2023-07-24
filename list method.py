0fa02768c4d48e7461feb579925b52b41dce9d80
github_username <you@example.com>
2023-07-24 18:30:20 +0500

    code for list methods

---
+++ list method.py
@@ -0,0 +1,63 @@
+"""fruits = ["apple", "banana", "cherry"]
+
+fruits.append("orange")
+
+    print(fruits)"
+
+
+
+province = ["kabul","bamyan","herat","logar","parwan"]
+province.clear()
+
+print(province)""
+
+
+
+fruits = ["apple","banana","mangoo","watermelon"]
+x =fruits.copy()
+print(x)""
+
+
+
+province = ["kabul","bamyan","herat","logar","parwan","kabul","kabul"]
+x = province.count("kabul")
+
+print(x)""
+
+
+province =["kabul","bamyan","herat","parwan"]
+
+cars = ["volvo","BMW","toyota",]
+province.extend(cars)
+print(province)""
+
+
+cars =["volvo","BMW","toyata"]
+x= cars.index("volvo")
+print(x)""
+
+
+cars= ["volvo","BMW","toyoto"]
+cars.insert(3,"orange")
+print(cars)""
+
+
+cars=["volvo","BMW","toyota"]
+cars.pop()
+print(cars)""
+
+
+Cars=["volvo","BMW","toyota"]
+Cars.remove("BMW")
+print(Cars)""
+
+
+province=["kabul","bamyan","herat","logar"]
+province.reverse()
+print(province)"""
+
+
+
+province=["kabul","bamyan","herat","logar"]
+province.sort()
+print(province)
