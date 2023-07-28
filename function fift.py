2ce862a42b5fbbcac7fb0833ea3c44cfe837c6ee
github_username <you@example.com>
2023-07-28 23:07:31 +0500

    assingment

--- function fift.py
+++ function fift.py
@@ -1,8 +1,8 @@
 def max_of_two(num1, num2):
     if num1 > num2:
-        print ("num1")
+        print ("this is numbr one",num1)
     else:
-        print ("num2")
+        print ("this is number two",num2)
 
 num1 = 32
 num2 = 78
